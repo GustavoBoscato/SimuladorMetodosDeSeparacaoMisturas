7071e90e7b01113f844785bfc616e6e345a0288d
Gustavo Boscato <curiosoxcdf@gmail.com>
2025-04-26 18:42:37 -0300

    LaboratorioModoDificil

--- app/src/Components/Principal/Laboratorio.jsx
+++ app/src/Components/Principal/Laboratorio.jsx
@@ -2,12 +2,33 @@ import React from 'react'
 import ListaMistura from './ListaMistura'
 import { Mistura } from '../../data/Mistura';
 import { agua, areia } from '../../data/dataComponentes';
-
+import './Laboratorio.css';
+import IconeVoltar from '../../img/IconeVoltar.png';
+import BicoDeEnyemeyer from '../../img/frascoLaboratorio.png' ;
+import Botao from './Botao';
+import SelectMetodos from './SelectMetodos';
 const Laboratorio = () => {
     const mistura1 = new Mistura([agua, areia]);
   return (
     <div className='laboratorio'>
         <ListaMistura h6='Mistura 01' mistura={mistura1}/>
+        <div className="laboratorioOpcoes">
+            <div className="topo">
+            <img src={IconeVoltar} alt="Imagem voltar" className='imagemVoltar' />
+                <h2>Simulação</h2>
+                <p id='modoNome'>Modo Difícil</p>
+            </div>
+            <div className="conteinerFrasco">
+              <img src={BicoDeEnyemeyer} alt="FrascoLaboratorio" className='FrascoLaboratorio' draggable="false" />
+            </div>
+            <div className="botoesLaboratorio">
+              <Botao colorFundo='#000' colorTexto='#FDFDFD' texto='Separar'/>
+              <SelectMetodos id='metodosModoLivre'/>
+              <Botao colorFundo='#484D50' colorTexto='#FDFDFD' texto='Opções'/>
+              
+            </div>
+                       
+        </div>
     </div>
   )
 }
